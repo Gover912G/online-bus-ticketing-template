4392631a8ffd43342c09c4da2442ccde2be9b383
Gover912G <ngigipaul912@gmail.com>
2024-01-31 17:27:15 +0300

    sign up form

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,29 +9,29 @@ var counter = 0;
 
 // Adding click event to the indicators
 
-function switchText(currentTest){
+function switchText(currentTest) {
     currentTest.classList.add('active');
     var testId = currentTest.getAttribute('attr');
-    if (testId > counter){
+    if (testId > counter) {
         testSlide[counter].style.animation = 'next1 0.5s ease-in forwards';
         counter = testId;
         testSlide[counter].style.animation = 'next2 0.5s ease-in forwards';
-        
+
     }
-    else if(testId==counter){return;}
-    else{
+    else if (testId == counter) { return; }
+    else {
         testSlide[counter].style.animation = 'prev1 0.5s ease-in forwards';
         counter = testId;
         testSlide[counter].style.animation = 'prev2 0.5s ease-in forwards';
-        
+
     }
     indicators();
 }
 
 // add and remove active class from indicators
 
-function indicators(){
-    for(i=0; i<dots.length; i++){
+function indicators() {
+    for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(' active', '');
     }
     dots[counter].className += ' active';
@@ -39,20 +39,20 @@ function indicators(){
 
 // code for auto sliding
 
-function slideNext(){
+function slideNext() {
     testSlide[counter].style.animation = 'next1 0.5s ease-in forwards';
-    if(counter >= testSlide.length - 1){
+    if (counter >= testSlide.length - 1) {
         counter = 0;
     }
     else {
-        counter ++
+        counter++
     }
     testSlide[counter].style.animation = 'next2 0.5s ease-in forwards';
 
 }
-function autoSliding(){
+function autoSliding() {
     deleteInterval = setInterval(timer, 2000);
-    function timer(){
+    function timer() {
         slideNext();
         indicators();
     }
@@ -62,10 +62,13 @@ autoSliding();
 // Stop auto sliding when mouse is over the indicators
 const container = document.querySelector('.indicators');
 container.addEventListener('mouseover', pause);
-function pause(){
+function pause() {
     clearInterval(deleteInterval);
 }
 
 //  Resume sliding when mouse is out of the indicators
 container.addEventListener('mouseout', autoSliding);
+
+
+
 
